4527b4f5f94597e982261e9dfe5412f426047c42
Jaya Simha Reddy <jayasimhareddy738@gmail.com>
2020-06-27 22:24:26 +0530

    Fixed Infinite Scroll

--- src/Store/configStore.js
+++ src/Store/configStore.js
@@ -1,14 +1,32 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import _ from "lodash";
+
 import configReducer from "./reducers/configReducer";
 import cartReducer from "./reducers/cartReducer";
 import productReducer from "./reducers/productReducer";
 import restaurantReducer from "./reducers/restaurantReducer";
+
+import { loadState, saveState } from "./localStorage";
+
+const persistedState = loadState();
+
 const rootReducer = combineReducers({
   config: configReducer,
   cart: cartReducer,
   product: productReducer,
   restaurant: restaurantReducer,
 });
-const Store = createStore(rootReducer, applyMiddleware(thunk));
+
+const Store = createStore(rootReducer, persistedState, applyMiddleware(thunk));
+
+Store.subscribe(
+  _.throttle(() => {
+    saveState({
+      cart: Store.getState().cart,
+      config: Store.getState().config,
+    });
+  }, 1000)
+);
+
 export default Store;
