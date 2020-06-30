7ff0c56d9519167c0aa29c2c5923fcf71bd952cb
Jaya Simha Reddy <jayasimhareddy738@gmail.com>
2020-06-30 22:59:55 +0530

    added build

---
+++ build/precache-manifest.61b2aaa51e607fcbabba892b6e02f000.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7368be5ff19a9dd979fcaf61a47c54e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9316478207e53a23b34",
+    "url": "/static/css/main.6dc10cb6.chunk.css"
+  },
+  {
+    "revision": "2ac7e077402a694128d5",
+    "url": "/static/js/2.6ab46805.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.6ab46805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9316478207e53a23b34",
+    "url": "/static/js/main.c3cbca57.chunk.js"
+  },
+  {
+    "revision": "f52171dc54b70b063a41",
+    "url": "/static/js/runtime-main.0c495077.js"
+  },
+  {
+    "revision": "4fc1b56e613eecccf3a1dd88000a791b",
+    "url": "/static/media/dark-logo.4fc1b56e.svg"
+  },
+  {
+    "revision": "b0030b9d18e4658d6057f82f004ab343",
+    "url": "/static/media/img-14.b0030b9d.jpg"
+  },
+  {
+    "revision": "6048fc74c2d7e1bd8b0651b5e8d91fa8",
+    "url": "/static/media/img-5.6048fc74.jpg"
+  },
+  {
+    "revision": "4fc1b56e613eecccf3a1dd88000a791b",
+    "url": "/static/media/logo.4fc1b56e.svg"
+  },
+  {
+    "revision": "2a9740dda3bb7efa7598a9fa17d83b42",
+    "url": "/static/media/restaurant.2a9740dd.svg"
+  }
+]);
