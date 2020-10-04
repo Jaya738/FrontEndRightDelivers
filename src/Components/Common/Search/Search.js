b9ac31a631ee9b453f798dc729a188e0b91e708b
Jaya Simha Reddy <jayasimhareddy738@gmail.com>
2020-10-04 16:44:39 +0530

    Added search functionality in restaurants

--- src/Components/Common/Search/Search.js
+++ src/Components/Common/Search/Search.js
@@ -1,24 +1,46 @@
 import React, { useState } from "react";
 import "./search.css";
 
-export default function Search(props) {
+export default function Search({
+  displayText = "Search",
+  value = "",
+  handleSearch,
+  onClear,
+}) {
   const [expanded, setExpanded] = useState(false);
   return (
-    <div classNameName="wrap">
-      <form id="content">
-        <input
-          type="text"
-          name="input"
-          className={expanded ? "input square" : "input"}
-          id="search-input"
-        />
+    <div
+      className="restaurants-search-box d-flex"
+      style={{ justifyContent: "left", margin: "0px 16px" }}
+    >
+      <input
+        style={{
+          border: "none",
+          backgroundColor: "transparent",
+          color: "#2f4f4f",
+          fontSize: "16px",
+          borderRadius: "5px",
+        }}
+        type="text"
+        placeholder={displayText}
+        value={value}
+        onChange={handleSearch}
+      />
+      {value !== "" && (
         <button
-          type="reset"
-          className={expanded ? "search close" : "search"}
-          id="search-btn"
-          onClick={() => setExpanded(!expanded)}
-        ></button>
-      </form>
+          className="clear-field"
+          onClick={onClear}
+          style={{
+            border: "none",
+            backgroundColor: "#2f4f4f",
+            color: "white",
+            padding: "4px 20px",
+            borderRadius: "5px",
+          }}
+        >
+          Clear Filter
+        </button>
+      )}
     </div>
   );
 }
--- src/Components/Restaurants/RListNew.js
+++ src/Components/Restaurants/RListNew.js
@@ -7,31 +7,54 @@ import Header from "../Header/Header";
 import Spinner from "../Common/Spinner";
 import * as actionCreators from "../../Store/actions/index";
 import MblNavbar from "../Common/MblNavbar";
+import Search from "../Common/Search/Search";
 
 function RestaurantList(props) {
   const step = 8;
   const baseUrl = props.config.baseUrl;
   const history = useHistory();
   const [index, setIndex] = useState(0);
+  const [searchInput, setSearchInput] = useState("");
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(false);
   const [loadMore, setLoadMore] = useState(false);
+  const [loadedData, setLoadedData] = useState([]);
+  const handleSearch = (e) => {
+    setSearchInput(e.target.value);
+  };
+  //search
+  useEffect(() => {
+    const filteredData = loadedData.filter((data) => {
+      if (searchInput === "") {
+        return data;
+      }
+      if (
+        data.name.toLowerCase().includes(searchInput.toLowerCase()) ||
+        data.disc.toLowerCase().includes(searchInput.toLowerCase())
+      ) {
+        return data;
+      }
+    });
+    setItems(filteredData);
+  }, [searchInput]);
   const getData = () => {
     setIndex(index + step);
-    const newProds = props.restaurants.items ? props.restaurants.items.slice(index, index + step) : [];
+    const newProds = props.restaurants.items
+      ? props.restaurants.items.slice(index, index + step)
+      : [];
     setItems((prevState) => prevState.concat(newProds));
   };
 
   useEffect(() => {
     // if(props.restaurants.refreshRestaurants){
     //   setLoading(true)
-    //   loadRestaurants();   
+    //   loadRestaurants();
     // }
-    setLoading(true)
+    setLoading(true);
     loadRestaurants();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  
+
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
@@ -57,7 +80,7 @@ function RestaurantList(props) {
     "/?branch=" +
     props.config.curBranch.bid;
   const loadRestaurants = async () => {
-    setItems([])
+    setItems([]);
     const options = {
       method: "GET",
       headers: {
@@ -70,20 +93,21 @@ function RestaurantList(props) {
       //props.updateRestaurants(res);
       props.setLoadedRestaurants(res);
       setItems(res.shops);
+      setLoadedData(res.shops);
       setLoading(false);
     }
   };
   const noItems = (
     <div
       style={{
-        color: "#d30013",
+        color: "#2f4f4f",
         fontSize: "20px",
         padding: "40px",
         marginTop: "20%",
         textAlign: "center",
       }}
     >
-      No Restaurants in this Branch yet...
+      No Restaurants
     </div>
   );
   const loadSpinner = (
@@ -97,10 +121,25 @@ function RestaurantList(props) {
         <Header />
       </div>
       <div className="d-block d-sm-none">
-        <MblNavbar heading={props.match.params.service.charAt(0).toUpperCase() + props.match.params.service.slice(1) || "Restaurants"} back={pushBack} />
+        <MblNavbar
+          heading={
+            props.match.params.service.charAt(0).toUpperCase() +
+              props.match.params.service.slice(1) || "Shops"
+          }
+          back={pushBack}
+        />
       </div>
-    
+
       <div className="all-product-grid mar-15">
+        <Search
+          displayText={`Search ${
+            props.match.params.service.charAt(0).toUpperCase() +
+              props.match.params.service.slice(1) || "Shops"
+          }`}
+          handleSearch={handleSearch}
+          onClear={() => setSearchInput("")}
+          value={searchInput}
+        />
         {items.length > 0 ? (
           <div className="container">
             <div className="row">
