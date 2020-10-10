b9ea5a5e343f1f62755ab2574dfe3db580cd3086
Jaya Simha Reddy <jayasimhareddy738@gmail.com>
2020-10-10 23:13:58 +0530

    Implemented Time Slots functionality

--- src/Components/Slots/Slots.js
+++ src/Components/Slots/Slots.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Slots.css';
 import { Dropdown, DropdownButton } from 'react-bootstrap';
 
@@ -6,27 +6,70 @@ function Slots() {
     const timeSlots = {
         "1" : {
             id: "1",
-            slot: "11:30 to 1:00 PM" 
+            slot: "11:30 AM to 1:00 PM" 
         },
         "2" : {
             id: "2",
-            slot: "3:00 to 5:00 PM" 
+            slot: "3:00 PM to 5:00 PM" 
         },
         "3" : {
             id: "3",
-            slot: "5:30 to 7:30 PM" 
+            slot: "5:30 PM to 7:30 PM" 
         }
     }
     const [availableSlots, setAvailableSlots] = useState(["1","2","3"]);
     const [selectedTimeSlot, setSelectedTimeSlot] = useState("1");
     const [selectedDay, setSelectedDay] = useState("Today");
-    const [schedule,setSchedule] = useState(false)
+    const [scheduled,setScheduled] = useState(false)
+    const [isTodayAvailable, setIsTodayAvailable] = useState(true)
+
+    useEffect(()=>{
+        const d = new Date();
+        const curHr = d.getHours();
+        const curMin = d.getMinutes();
+        if(curHr < 18 && curMin < 30 ){
+            setIsTodayAvailable(true)
+        }
+        else{
+            setIsTodayAvailable(false)
+            setSelectedDay("Tomorrow")
+            setAvailableSlots(["1","2","3"])
+            setSelectedTimeSlot("1")
+        }
+    },[])
+    useEffect(()=>{
+        const d = new Date();
+        const curHr = d.getHours();
+        const curMin = d.getMinutes();
+
+        if(selectedDay === "Today"){
+            if(curHr < 12 && curMin < 30){
+                setAvailableSlots(["1","2","3"])
+                setSelectedTimeSlot("1")
+            }
+            else if(curHr > 11 && curHr < 15){
+                setAvailableSlots(["2","3"])
+                setSelectedTimeSlot("2")
+            }
+            else{
+                setAvailableSlots(["3"])
+                setSelectedTimeSlot("3")
+            }
+        }
+        else{
+            setAvailableSlots(["1","2","3"])
+            setSelectedTimeSlot("1")
+        }
+        
+        
+    },[selectedDay])
+    
     return (
         <div className="slot-selector">
             <div className="slot-header">
                 <div className="slot-switch">
                     <label className="switch">
-                        <input type="checkbox" checked={schedule} onChange={() => setSchedule(!schedule)} />
+                        <input type="checkbox" checked={scheduled} onChange={() => setScheduled(!scheduled)} />
                         <span className="slider round"></span>
                     </label>
                 </div>
@@ -34,7 +77,8 @@ function Slots() {
                     Schedule Delivery
                 </div>
             </div>
-            {schedule && (<div className="slot-body">
+            {scheduled && (
+            <div className="slot-body">
                 <div className="slot-time">
                     <DropdownButton size="lg" key="down" drop="down" title={timeSlots[selectedTimeSlot].slot}>
                         {availableSlots.map((slot) => (
@@ -44,11 +88,12 @@ function Slots() {
                 </div>
                 <div className="slot-day">
                     <DropdownButton size="lg" key="down" drop="down" title={selectedDay}>
-                        <Dropdown.Item onClick={() => setSelectedDay("Today")}>Today</Dropdown.Item>
+                        {isTodayAvailable && <Dropdown.Item onClick={() => setSelectedDay("Today")} >Today</Dropdown.Item>}
                         <Dropdown.Item onClick={() => setSelectedDay("Tomorrow")}>Tomorrow</Dropdown.Item>
                     </DropdownButton>
                 </div>
-            </div>)}
+            </div>
+            )}
         </div>
 
     )
